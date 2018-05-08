9ef2e7e12cdf2a1bbad9fd67d900a2fa3ee9b3b6
Andre Chaves <andrechavesg@gmail.com>
2018-05-08 17:28:06 -0300

    capitulo2

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -1,23 +1,46 @@
 module.exports = function(grunt) {
+
    grunt.initConfig({
-        copy: {
-              public: {
-                   cwd: 'public', 
-                   src: ['**'], 
-                   dest: 'dist', 
-                   expand: true
-              }
-         }, 
-         clean: {
-              dist: {
-                  src: ['dist']
-              }
+      /* Copia os arquivos para o diretório 'dist' */
+      copy: {
+         public: {
+           expand: true,
+           cwd: 'public',
+           src: '**',
+           dest: 'dist'
          }
+     },
+
+     clean: {
+          dist: {
+              src: 'dist'
+          }
+     },
+
+     useminPrepare: {
+       html: 'dist/**/*.html'
+     },
+
+     usemin: {
+       html: 'dist/**/*.html'
+     }
   });
 
-  grunt.registerTask('default', ['dist']);
+  //registrando task para minificação
+
   grunt.registerTask('dist', ['clean', 'copy']);
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('minifica', ['useminPrepare', 
+                                  'concat', 'uglify', 'cssmin', 'usemin']);
+
+  // registrando tasks
+  grunt.registerTask('default', ['dist', 'minifica']);
+
+  // carregando tasks
+  grunt.loadNpmTasks('grunt-contrib-copy'); 
   grunt.loadNpmTasks('grunt-contrib-clean');
-};
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-usemin');
+}
